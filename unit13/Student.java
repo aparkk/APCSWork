47789c93cb529582e709d66bc1ccee44cda228bf
aparkk <avanarapark@gmail.com>
2023-03-02 22:27:48 -0800

    unit 13 assignment

---
+++ unit13/Student.java
@@ -0,0 +1,19 @@
+//
+// Student.java
+//
+
+
+public class Student
+{
+  public Student(String name, int favNum)
+  {
+    this.name = name;
+    this.favNum = favNum;
+  }
+
+  public String getName() {return name;}
+  public int getFavNum() {return favNum;}
+
+  private String name;
+  private int favNum;
+}
